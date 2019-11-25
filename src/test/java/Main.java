b0ca03473c098aa41d76beb5c97b9159b6124d47
yuit <1239964852g@gmail.com>
2019-11-25 17:53:40 +0800

    commit: 优化

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,6 +1,11 @@
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.security.core.parameters.P;
 import org.springframework.util.AntPathMatcher;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @author yuit
@@ -10,17 +15,94 @@ import org.springframework.util.AntPathMatcher;
  */
 public class Main {
 
-    public static void main(String[] args){
 
 
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Integer> res = new LinkedList<>();
+        Producer p =  new Producer(res);
+        Consumer c = new Consumer(res);
+
+        Thread pth = new Thread(p);
+        Thread cth = new Thread(c);
+
+        pth.start();
+        cth.start();
+
+        pth.join();
+        cth.join();
 
     }
 
+
 }
 
-/**
+class Producer implements Runnable {
+
+    private Queue<Integer> res;
+
+    public Producer(Queue<Integer> res) {
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+
+       synchronized (res){
+           if (res.size()>=5) {
+               res.notify();
+               try {
+                   res.wait();
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }
+
+           res.add(1);
+           res.notify();
+           try {
+               Thread.sleep(1*1000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+
+           System.out.println("Producer: "+ res);
+
+           run();
+       }
+
+    }
+}
 
 
+class Consumer implements Runnable {
+    private Queue<Integer> res;
 
+    public Consumer(Queue<Integer> res) {
+        this.res = res;
+    }
 
- */
+    @Override
+    public void run() {
+        synchronized (res){
+            if (res.size()==0) {
+                res.notify();
+                try {
+                    res.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            res.poll();
+            res.notify();
+            try {
+                Thread.sleep(1*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Consumer: "+ res);
+
+            run();
+        }
+    }
+}
